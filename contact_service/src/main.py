87d7a774bf82c617d47fd7e7bf53560cd15a5a87
procergs\rafael-fanti <rafael-fanti@procergs.rs.gov.br>
2025-06-07 11:43:14 -0300

    swagger

--- contact_service/src/main.py
+++ contact_service/src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from routes import router
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Contatos",
+    description="Uma API simples de agenda de contatos com Swagger UI",
+    version="1.0.0"
+)
+
 app.include_router(router)
